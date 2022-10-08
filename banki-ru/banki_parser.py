b061f42d506061389a6a406c8556a48ceea4a7ac
Darinochka <darisha6602@gmail.com>
2022-10-08 17:25:32 +0300

    Fix: changed getting content using selenium

--- banki-ru/banki_parser.py
+++ banki-ru/banki_parser.py
@@ -5,12 +5,15 @@ import requests
 import csv
 import toml
 import os
-
+from selenium import webdriver
 
 PROJECT_DIR = join(dirname(realpath(__file__)), "..")
 CONFIG_PATH = join(PROJECT_DIR, "config.toml")
 config = toml.load(CONFIG_PATH)
 
+options = webdriver.FirefoxOptions()
+options.add_argument('--headless')
+driver = webdriver.Firefox(options=options)
 
 def get_soup_news_date(year: int, month: int, day: int):
     """
@@ -29,16 +32,19 @@ def content_news(url: str):
     """
     Возвращает текст новости на странице url
     """
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
     code_html = 'l6d291019'
-    st = soup.find_all('div', class_=code_html)[0]
+    st = soup.find('div', class_=code_html)
 
     content = ""
-    for p in st.find_all('p'):
-        content += p.get_text()
+    if st is not None:
+        for p in st.find_all('p', recursive=False):
+            content += p.get_text().strip()
 
-    return content
+    return content.strip()
 
 def get_news(start: date, end: date, file_path: str):
     """
@@ -62,6 +68,11 @@ def get_news(start: date, end: date, file_path: str):
                 date = i.find_all('div', class_='l31caf777')[0].get_text().strip()
                 views_comments = i.find_all('span', class_='l1800f3ef')
                 views = views_comments[0].get_text().strip()
+                url = f"https://www.banki.ru{i.a['href']}"
+                content = content_news(url)
+
+                if not content:
+                    continue
 
                 if len(views_comments) > 1:
                     comments = views_comments[1].get_text().strip()
@@ -70,9 +81,9 @@ def get_news(start: date, end: date, file_path: str):
 
                 news = {
                     'title': i.a.get_text().strip(),
-                    'url': f"https://www.banki.ru{i.a['href']}",
+                    'url': url,
                     'date': date,
-                    'content': content_news(f"https://www.banki.ru{i.a['href']}"),
+                    'content': content,
                     'num_views': views,
                     'num_comments': comments,
                 }
@@ -90,7 +101,7 @@ if __name__ == "__main__":
     # раскомментить, если начальную дату берешь из файла
 
     # with open(dirname(realpath(__file__)) + '/end_date.txt', 'r') as f_date:
-        # start = date(*list(map(int, f_date.readline().split('-'))))
+    #     start = date(*list(map(int, f_date.readline().split('-'))))
 
     end = date.today()
     get_news(start, end, PROJECT_DIR + config['banki_ru_path'])
