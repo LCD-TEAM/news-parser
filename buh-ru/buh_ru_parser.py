2c047b293514e2f464eb3ea17eb6e2bced951bae
idprodma <idprodma@edu.hse.ru>
2022-10-08 11:09:13 +0300

    пофиксил парсер, чтобы не тягал мусор, добавил его в папку для унификации

--- buh-ru/buh_ru_parser.py
+++ buh-ru/buh_ru_parser.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import re
 from bs4 import BeautifulSoup
 from datetime import date, datetime
+from csv import DictWriter
 
 class DatetimeRange:
     def __init__(self, dt1, dt2):
@@ -34,23 +35,25 @@ options = webdriver.FirefoxOptions()
 options.add_argument('--headless')
 driver = webdriver.Firefox(options=options)
 
-def get_articles(dateFrom, dateTo):
-    dt_range = DatetimeRange(dateFrom, dateTo)
+def get_news(date_from, date_to):
+    dt_range = DatetimeRange(date_from, date_to)
     driver.get('https://buh.ru/news/uchet_nalogi/')
     html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'lxml')
     news_headers = soup.findAll('article', class_='article')
     news = []
     for news_header in news_headers:
         link = news_header.find('a')['href']
         driver.get(url + link)
         news_page = driver.page_source
-        news_soup = BeautifulSoup(news_page, 'html.parser')
+        news_soup = BeautifulSoup(news_page, 'lxml')
         date_str = news_soup.find('span', class_='grayd').get_text()
+        article_soup = news_soup.find('div', class_='tip-news', itemprop='articleBody')
         if datetime.strptime(date_str, '%d.%m.%Y').date() in dt_range:
             news.append({
+                'title': news_soup.find('h1', class_='margin_line-height phead specdiv').get_text(),
                 'date': date_str,
-                'content': '\n'.join(list(map(lambda t: t.get_text(), news_soup.find('div', class_='tip-news').findAll('p')))),
+                'content': article_soup.get_text().rsplit('\n\nОпрос')[0].rstrip(),
                 'num_views': _get_num_views(news_page),
                 'num_comments': _get_num_comments(news_soup),
                 'tags': _get_tags(news_soup)
@@ -59,3 +62,17 @@ def get_articles(dateFrom, dateTo):
             break
     driver.quit()
     return news
+
+def write_news(path, news):
+    keys = news[0].keys()
+
+    with open(path, 'w', encoding='utf-8') as file:
+        writer = DictWriter(file, keys)
+        writer.writeheader()
+        writer.writerows(news)
+
+if __name__ == '__main__':
+    start = date(2020, 10, 7)
+    end = date.today()
+    write_news('news_buhru.csv', get_news(start, end))
+    
